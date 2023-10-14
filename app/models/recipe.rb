7dca608f806c2349a7028dee3a78fbbd3bbf43fc
Christopher Winslett <chris.winslett@crunchydata.com>
2023-10-14 17:07:24 -0500

    add the basics of the hnsw index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,16 +3,15 @@ class Recipe < ApplicationRecord
   has_many :recipe_embeddings
 
   def recommended(limit = 5)
-    recipe_ids = RecipeEmbedding.find_by_sql([<<-SQL, self.id, limit]).map(&:recipe_id)
+    recipe_ids = RecipeEmbedding.find_by_sql([<<-SQL, self.id, limit + 1]).map(&:recipe_id)
     SELECT
       re2.recipe_id
     FROM recipe_embeddings AS re1, recipe_embeddings AS re2
     WHERE re1.recipe_id = ?
-      AND re1.recipe_id != re2.recipe_id
     ORDER BY re1.embedding <-> re2.embedding
     LIMIT ?
 SQL
 
-    Recipe.where(id: recipe_ids)
+    Recipe.where(id: recipe_ids - [self.id])
   end
 end
